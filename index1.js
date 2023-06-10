6c7c11f94a2a2c41b786f137a8d8a68239d9864e
Ragini <raginiv743@gmail.com>
2023-06-10 15:47:08 +0530

    episode 1

---
+++ index1.js
@@ -0,0 +1,16 @@
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I am h1"),
+        React.createElement("h2",{},"I am h2") , 
+      ]),
+      React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am h2 tag"),
+
+      ]),
+]);
+
+console.log(parent)
+let root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
