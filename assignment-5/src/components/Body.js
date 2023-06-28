5656d68c62c787ea98122bbde08441fce67aebcc
Ragini <raginiv743@gmail.com>
2023-06-28 23:26:55 +0530

    completed assignment

--- assignment-5/src/components/Body.js
+++ assignment-5/src/components/Body.js
@@ -1,22 +1,37 @@
 import RestaurantCard from "./RestaurantCard" ;
-import resList from "../utils/mockData.js"
+
+import resList from "../utils/mockData.js";
+
+import { useState } from "react";
+
+
 
 
 const Body = () => {
+  const [resListofRestaurants,setResList]=useState(resList)
+  console.log(resListofRestaurants)
+  console.log(resList)
   return (
     <div className="body">
       <div className="search">
-        <button className="filter-btn">top rated Restaurants</button>
+        <button className="filter-btn" onClick={()=> { const filterList=
+          resListofRestaurants.filter((res)=>res.data.avgRating>4)
+          setResList(filterList)
+          
+          }
+          }>top rated Restaurants</button>
     
       
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {resListofRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resdata={restaurant} />
         ))}
       </div>
     </div>
   );
 };
+
+// console.log(resListofRestaurants)
 
 export default Body
---
+++ assignment-5/theory/README.md
@@ -0,0 +1,37 @@
+### what is difference between named export and defaault export and *as export ? 
+
+1-named
+
+multiple things export from the file
+
+example-
+
+export const ComponentName
+
+import { ComponentName} from "path"
+
+
+2 - default
+
+one tthing export from the file
+
+example-
+
+export default ComponentName
+
+import Component from "path"
+### what are React Hooks ?
+react hooks are function provided that allows to use state and other react feature . 
+
+normal js utility functions
+
+there are two types of Hooks
+
+1- useState() -super powerful state variable in React
+
+2- useEffect()
+
+### what is importance of config.js file ?
+config.js file store to configuration setting or variable that are used throughout the application . 
+### why do we need a useState Hook ? 
+the useState Hooks is used to add state to functional component it allows you to manage and update state within a component .
